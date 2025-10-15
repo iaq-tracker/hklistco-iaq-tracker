a5f1a04b1266a4737d2f549c74bd9c41f3f8933b
ernesthung <ernesthungmh@gmail.com>
2025-10-16 03:24:23 +0800

    control order by market cap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1049,7 +1049,7 @@ def load_control_df():
     """
     Load ir_contacts table from database to session state
     """
-    response = supabase.table("control").select("*").order("stock_code").execute()
+    response = supabase.table("control").select("*").order("market_cap").execute()
     df = pd.DataFrame(response.data)
 
     # convert datetime fields
